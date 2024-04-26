50128d7eb5cd200cc76b1b8664c658e18aceb9be
DerpyTheSpy <erickim334@gmail.com>
2024-04-25 21:59:47 -0400

    Added Event handling component EventBind.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,15 +7,17 @@ import Message from './components/Message';
 import Counter from './components/Counter';
 import FunctionClick from './components/FunctionClick';
 import ClassClick from './components/ClassClick';
+import EventBind from './components/EventBind';
 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <FunctionClick />
+        <EventBind />
+        {/*<FunctionClick />
         <ClassClick />
-        {/*<Counter /> */}
+        <Counter /> */}
         {/*<Message /> */}
         {/*<Greet name="Bruce" heroName="Batman">
           <p>This is children props</p>
