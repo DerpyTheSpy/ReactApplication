c5055633a3f06bd0805c2862339561ea009da56d
DerpyTheSpy <erickim334@gmail.com>
2024-04-26 21:43:57 -0400

    added changes to NameList.js

--- my-app/src/components/NameList.js
+++ my-app/src/components/NameList.js
@@ -1,28 +1,32 @@
 import React from 'react'
+import Person from './Person'
+
+// key is used to identify the element in the list
 
 function NameList() {
+    const names = ['Bruce', 'Clark', 'Diana', 'Bruce']
     const persons = [
         {
             id: 1,
-            name: 'John',
+            name: 'Bruce',
             age: 30,
             skill: 'React'
         },
         {
             id: 2,
-            name: 'Kim',
+            name: 'Clark',
             age: 25,
             skill: 'Angular'
         },
         {
             id: 3,
-            name: 'Peter',
-            age: 30,
+            name: 'Diana',
+            age: 28,
             skill: 'Vue'
         }
     ]
-    const personList = persons.map(person => <h2>I am {person.name}. I am {person.age} years old. I know {person.skill}</h2>)
-    return <div>{personList}</div>
+    const nameList = names.map((name, index) => <h2 key={index}>{index} {name}</h2>)
+    return <div>{nameList}</div>
 }
 
 export default NameList
